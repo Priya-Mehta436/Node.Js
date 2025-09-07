df60e46e0a3cb880daba3b443549b3c9f6297418
Priya Mehta <pmehta12122005@gmail.com>
2025-09-07 21:49:05 +0530

    POST requests in express

--- Part08_ExpressProject/server.js
+++ Part08_ExpressProject/server.js
@@ -5,10 +5,14 @@ const app = express();
 const PORT = 3000;
 
 const friends = [
-  { id: 0,
-    name: 'Albert Einstein' },
-  { id: 1, 
-    name: 'Sir Isaac Newton' },
+  {
+     id: 0,
+    name: 'Albert Einstein' 
+  },
+  { 
+    id: 1, 
+    name: 'Sir Isaac Newton' 
+  },
 ];
 
 app.use((req, res, next)=> {
@@ -16,6 +20,23 @@ app.use((req, res, next)=> {
   next();
   const delta = Date.now() - start;
   console.log(`${req.method} ${req.url} ${delta}ms`)
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res)=> {
+  if(!req.body.name){
+    res.status(400).json({
+      error: 'Missing friend name'
+    })
+  }
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length 
+  };
+  friends.push(newFriend);
+
+  res.json(newFriend);
 });
 
 app.get('/friends', (req, res) => {
@@ -30,7 +51,7 @@ app.get('/friends/:friendId', (req, res) => {
     res.status(200).json(friend);
   } else {
     res.status(404).json({
-      error: "Friend does not exist"
+      error: 'Friend does not exist'
     }); 
   }
 });
