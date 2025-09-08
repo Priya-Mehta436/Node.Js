0b16c4e3fd3d4450000c9121c8f24614dedb40b2
Priya Mehta <pmehta12122005@gmail.com>
2025-09-08 12:41:56 +0530

    Layouts and Separation of concerns

--- Part08_ExpressProject/Controllers/messages.controller.js
+++ Part08_ExpressProject/Controllers/messages.controller.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 function getMessages (req, res) {
     res.sendFile( path.join(__dirname,'..', 'public', 'skimountain.jpg' ));
-//   res.send('<ul><li>Hello Albert!</li></ul>')
 }
 
 function postMessage (req, res) {
