bda6afc2b5229ae3beed72272e9598f4e7aa3ace
Priya Mehta <pmehta12122005@gmail.com>
2025-09-14 11:17:45 +0530

    Loading Data On Startup

--- Part09_NASA Project/server/src/app.js
+++ Part09_NASA Project/server/src/app.js
@@ -1,3 +1,4 @@
+const  path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
@@ -8,6 +9,6 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
-app.use(planetsRouter);
+app.use('/planets', planetsRouter);
 
 module.exports = app;  
--- Part09_NASA Project/server/src/server.js
+++ Part09_NASA Project/server/src/server.js
@@ -2,14 +2,20 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+async function startServer() {
+  
+await loadPlanetsData();
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
-});
+ });
+}
 
-app.listen();
-
-console.log(PORT);
+startServer();
+// ....
