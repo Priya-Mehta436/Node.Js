50ea37c5aa1dd9bf331b455c23479168be11b58e
Priya Mehta <pmehta12122005@gmail.com>
2025-10-03 19:19:50 +0530

    NASA project

--- Part09_NASA Project/server/src/models/planets.model.js
+++ Part09_NASA Project/server/src/models/planets.model.js
@@ -34,8 +34,11 @@ return new Promise((resolve, reject) => {
         });
     });
 }
+function getAllPlanets() {
+    return habitablePlanets;
+}
 
   module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets,
+    getAllPlanets,
   };
